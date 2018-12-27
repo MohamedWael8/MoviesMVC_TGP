202e63002b04cf45a4e24a6dad2f4a460bb059c5
Mohamed Wael <Mohamed.Wael@itworx.com>
2018-12-27 16:32:08 +0200

    Added AddMovie View

--- TGP.MVC.Movies/TGP.MVC.Movies/Controllers/HomeController.cs
+++ TGP.MVC.Movies/TGP.MVC.Movies/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace TGP.MVC.Movies.Controllers
             return View();
         }
 
+        public ActionResult Movie()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
